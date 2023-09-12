26f1c460404b86f0d24fb4f81deb486c7a743d33
Taj <tajmarie92@gmail.com>
2023-09-11 23:11:23 -0500

    Completed Number Guessing Game Selection Statements.

--- SelectionStatementExercise/Program.cs
+++ SelectionStatementExercise/Program.cs
@@ -4,7 +4,25 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            Console.WriteLine("Let's play a guessing game! Give me a number between 1 and 1,000!");
+
+            var r = new Random();
+            var favNumber = r.Next(1, 1000);
+            int userInput = int.Parse(Console.ReadLine());
+
+            if (userInput < 709)
+            {
+                Console.WriteLine("Too low..");
+            }
+            else if (userInput > 709)
+            {
+                Console.WriteLine("Too High");
+            }
+            else if(userInput == 709)
+            {
+                Console.WriteLine("You guessed it!");
+            }            
         }
     }
+
 }
